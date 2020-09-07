f7a670189ac12b322ca8ebc878473a805f1a58e6
Lorena Arce <you@example.com>
2020-09-07 01:50:34 -0700

    testing different functions

--- PyBank/main.py
+++ PyBank/main.py
@@ -8,23 +8,47 @@
 
 
 #import module
-import os
+#import os
 #import module to read csv files
-import csv
+#import csv
 
 #path to collect data from excel file
 #budget_csv = os.path.join('python-challenge/PyBank/Resources/budget_data.csv')
-budget_csv = os.path.join('C:\\Users\\loren\\Desktop\\python-challenge\\PyBank\\Resources\\budget_data.csv')
+#budget_csv = os.path.join('C:\\Users\\loren\\Desktop\\python-challenge\\PyBank\\Resources\\budget_data.csv')
 
 #open the csv
-with open (budget_csv) as csvfile:
-    csvreader = csv.reader(csvfile, delimiter=",")
+#with open (budget_csv) as csvfile:
+    #csvreader = csv.reader(csvfile, delimiter=",")
     #print(csvreader)
 
 #read header row first
-    csvheader = next(csvreader)
+    #csvheader = next(csvreader)
     #print(f'csv header: {csvheader}')
 
 #read each row after the header
     #for row in csvreader:
         #print(row)
+
+#import csv
+
+#f = open('C:\\Users\\loren\\Desktop\\python-challenge\\PyBank\\Resources\\budget_data.csv', 'r')
+
+#reader = csv.reader(f)
+
+#budget = {}
+
+#for row in reader:
+    #{'Date': row[0], 'Profit/Losses':row[1]}
+    #print(row)
+
+import csv
+
+with open('C:\\Users\\loren\\Desktop\\python-challenge\\PyBank\\Resources\\budget_data.csv', 'r') as csvfile:
+    csv_dict_reader = csv.DictReader(csvfile)
+
+    for row in csv_dict_reader:
+        print(str(len(row['Date'])))
+
+
+
+
