d677f452bd282004b1ca38739d053a7f968d9e5e
Lorena Arce <you@example.com>
2020-09-07 20:32:27 -0700

    almost done, need to fix average

--- PyBank/main.py
+++ PyBank/main.py
@@ -34,29 +34,80 @@
 #f = open('C:\\Users\\loren\\Desktop\\python-challenge\\PyBank\\Resources\\budget_data.csv', 'r')
 
 #reader = csv.reader(f)
-
+#csvheader = next(reader)
 #budget = {}
 
 #for row in reader:
     #{'Date': row[0], 'Profit/Losses':row[1]}
-    #print(row)
+    #print(row[0])
+    #months=len(reader)
+    #print(months)
 
+#import csv
+
+#total_months = 0
+#total_profit_and_losses = 0
+
+
+
+#with open('C:\\Users\\loren\\Desktop\\python-challenge\\PyBank\\Resources\\budget_data.csv', 'r') as csvfile:
+    #reader = csv.DictReader(csvfile)
+    
+
+    #for row in reader:
+        #total_months = total_months + 1
+        #total_profit_and_losses = total_profit_and_losses + int(row['Profit/Losses'])
+        #count = count + 1
+        
+        #average_change = profits - past_profits
+#print(total_months)
+#print(total_profit_and_losses)
+#print(average_change)
+
+import os
 import csv
 
 total_months = 0
 total_profit_and_losses = 0
+past_profit_loss = 0
+profit_and_loss_change = 0
+changes = []
+greatest_increase_profits = ['', 0]
+greatest_decrease_losses = ['', 9999999]
+
 
 with open('C:\\Users\\loren\\Desktop\\python-challenge\\PyBank\\Resources\\budget_data.csv', 'r') as csvfile:
     reader = csv.DictReader(csvfile)
     
+
     for row in reader:
-        #total_months = total_months + 1
+    
+        total_months = total_months + 1
+        #print(total_months)
         total_profit_and_losses = total_profit_and_losses + int(row['Profit/Losses'])
-#print(total_months)
+        #print(total_profit_and_losses)
+        profit_and_loss_change = int(row['Profit/Losses']) - past_profit_loss
+        #print(profit_and_loss_change)
+        past_profit_loss = int(row['Profit/Losses'])
+        #print(past_profit_loss)
+
+        if profit_and_loss_change > greatest_increase_profits[1]:
+            greatest_increase_profits[1]= profit_and_loss_change
+            greatest_increase_profits[0]= row['Date']
+
+        if profit_and_loss_change < greatest_decrease_losses[1]:
+            greatest_decrease_losses[1] = profit_and_loss_change
+            greatest_decrease_losses[0] = row['Date']
+        changes.append(int(row['Profit/Losses']))
+        #print(average_change)
+    average_change = sum(changes)/len(changes)
+print(total_months)
 print(total_profit_and_losses)
-
-
-
-
-
-
+#print(average_change)
+#print(months)
+#print(profit_and_loss_change)
+print(str(round(sum(changes) / len(changes),2)))
+print(str(greatest_increase_profits[0]))
+print(str(greatest_increase_profits[1]))
+print(str(greatest_decrease_losses[0]))
+print(str(greatest_decrease_losses[1]))
